fd13010f7197946ca0ce0ab33a77346e8ad54bbf
phatommobb <nickoluoch1130@gmail.com>
2025-07-29 13:53:05 +0300

    code

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ const moment = require("moment");
 console.log(moment().startOf("hour").fromNow());
 console.log(moment().startOf("day").fromNow());
 console.log(moment().endOf("day").fromNow());
+console.log(x);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,22 @@
 module.exports = {
+  mode: "development",
   entry: "./index.js",
   output: {
     filename: "bundle.js",
+  },
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+          },
+        },
+      },
+    ],
   },
 };
