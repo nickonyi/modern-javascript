93c8820035be0746cb796361ca0d3c34d4a83feb
phatommobb <nickoluoch1130@gmail.com>
2025-07-29 14:26:24 +0300

    webpack dev serever config

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,12 @@ module.exports = {
     publicPath: "/",
   },
   devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 8080,
+    open: true,
+  },
 };
